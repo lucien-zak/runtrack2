40531aa409a9f0033f43d6df4f84c2a78a832f8f
[lucien-zak] <lucienzak@MacBook-Air-de-Lucien.local>
2021-11-04 08:56:40 +0100

    modification script premier

--- jour02/job05/index.php
+++ jour02/job05/index.php
@@ -3,10 +3,9 @@ $j = 1;
 $divisible = 0;
 
 for ($i = 2;$i <= 1000;$i++) {
-    while ($j + 1 < $i ) {
+    while ($j + 1 < $i - 1 ) {
         $j += 1;
         if ($i % $j == 0) {
-            echo "";
             $divisible += 1;
         }
     }
