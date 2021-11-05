37ab467a058a74bace75435f0d8ab3ed91b25320
[lucien-zak] <lucienzak@MacBook-Air-de-Lucien.local>
2021-11-05 09:04:55 +0100

    code raccourci

--- jour03/job07/index.php
+++ jour03/job07/index.php
@@ -2,7 +2,9 @@
 
 $str = "Certaines choses changent, et d'autres ne changeront jamais.";
 for ($compteurcar = 0; isset($str[$compteurcar]); $compteurcar++) {}
-for ($i = 0; $i <= $compteurcar ; $i++) {
-    echo $str[$i+1];
+echo $str."<br>";
+for ($i = 1; $i <= $compteurcar - 2 ; $i++) {
+    echo $str[$i];
 }
+    echo $str[$compteurcar - 1].$str[0];
 ?>
