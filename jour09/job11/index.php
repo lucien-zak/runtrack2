219d244ff7030d1e30856da00874c2648cdc8e3c
[lucien-zak] <lucienzak@MacBook-Air-de-Lucien.local>
2021-11-12 12:20:35 +0100

    ajout jour09

---
+++ jour09/job11/index.php
@@ -0,0 +1,42 @@
+<?php
+function recup_nom_array()
+{
+    $db = mysqli_connect("localhost", "root", "root", "jour08");
+    $req = mysqli_query($db, "SELECT AVG(capacite) as capacite_moyenne FROM `salles`");
+    $res = mysqli_fetch_all($req, MYSQLI_ASSOC);
+    foreach ($res as $element) {
+        foreach ($element as $key => $element2) {
+            echo "<th>" . $key . "</th>";
+        }
+        break;
+    }
+}
+
+function recup_ligne()
+{
+    $db = mysqli_connect("localhost", "root", "root", "jour08");
+    $req = mysqli_query($db, "SELECT AVG(capacite) as capacite_moyenne FROM `salles`");
+    $res = mysqli_fetch_all($req, MYSQLI_ASSOC);
+    foreach ($res as $element) {
+        echo "<tr>";
+        foreach ($element as $key => $element2) {
+            echo "<td>" . $element2 . "</td>";
+        }
+        echo "</tr>";
+    }
+}
+?>
+<style>
+    td, th {border-spacing: 0; border: 1px solid black; padding: 10px; text-align: center;}
+    table {border-spacing: 0;}
+</style>
+<table>
+    <thead>
+        <tr>
+            <?php recup_nom_array() ?>
+        </tr>
+    </thead>
+    <tbody>
+        <?php recup_ligne()?>
+    </tbody>
+</table>
